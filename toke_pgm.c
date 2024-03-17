0fc29ef2c406c32d931a461f017167cf0b9ae08e
Mayank Joshi <80774461+Mayank-Joshi525@users.noreply.github.com>
2024-03-17 10:51:40 +0530

    Update toke_pgm.c

--- toke_pgm.c
+++ toke_pgm.c
@@ -1,32 +1,22 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
 bool isDelimiter(char ch) {
     if (ch == ' ' || ch == '+' || ch == '-' || ch == '*' ||
         ch == '/' || ch == ',' || ch == ';' || ch == '>' ||
         ch == '<' || ch == '=' || ch == '(' || ch == ')' ||
         ch == '[' || ch == ']' || ch == '{' || ch == '}')
-        return (true);
-    return (false);
+        return true;
+    return false;
 }
 
 bool isOperator(char ch) {
     if (ch == '+' || ch == '-' || ch == '*' ||
         ch == '/' || ch == '>' || ch == '<' ||
         ch == '=')
-        return (true);
-    return (false);
-}
-
-bool validIdentifier(char *str) {
-    if (str[0] == '0' || str[0] == '1' || str[0] == '2' ||
-        str[0] == '3' || str[0] == '4' || str[0] == '5' ||
-        str[0] == '6' || str[0] == '7' || str[0] == '8' ||
-        str[0] == '9' || isDelimiter(str[0]) == true)
-        return (false);
-    return (true);
+        return true;
+    return false;
 }
 
 bool isKeyword(char *str) {
@@ -42,92 +32,63 @@ bool isKeyword(char *str) {
         || !strcmp(str, "switch") || !strcmp(str, "unsigned")
         || !strcmp(str, "void") || !strcmp(str, "static")
         || !strcmp(str, "struct") || !strcmp(str, "goto"))
-        return (true);
-    return (false);
+        return true;
+    return false;
 }
 
-bool isInteger(char *str) {
-    int i, len = strlen(str);
-
-    if (len == 0)
-        return (false);
-    for (i = 0; i < len; i++) {
-        if (str[i] != '0' && str[i] != '1' && str[i] != '2'
-            && str[i] != '3' && str[i] != '4' && str[i] != '5'
-            && str[i] != '6' && str[i] != '7' && str[i] != '8'
-            && str[i] != '9' || (str[i] == '-' && i > 0))
-            return (false);
-    }
-    return (true);
-}
-
-bool isRealNumber(char *str) {
-    int i, len = strlen(str);
-    bool hasDecimal = false;
-
-    if (len == 0)
-        return (false);
-    for (i = 0; i < len; i++) {
-        if (str[i] != '0' && str[i] != '1' && str[i] != '2'
-            && str[i] != '3' && str[i] != '4' && str[i] != '5'
-            && str[i] != '6' && str[i] != '7' && str[i] != '8'
-            && str[i] != '9' && str[i] != '.' ||
-            (str[i] == '-' && i > 0))
-            return (false);
-        if (str[i] == '.')
-            hasDecimal = true;
-    }
-    return (hasDecimal);
-}
-
-char *subString(char *str, int left, int right) {
-    int i;
-    char *subStr = (char *)malloc(
-        sizeof(char) * (right - left + 2));
-
-    for (i = left; i <= right; i++)
-        subStr[i - left] = str[i];
-    subStr[right - left + 1] = '\0';
-    return (subStr);
+bool validIdentifier(char *str) {
+    if (str[0] == '0' || str[0] == '1' || str[0] == '2' ||
+        str[0] == '3' || str[0] == '4' || str[0] == '5' ||
+        str[0] == '6' || str[0] == '7' || str[0] == '8' ||
+        str[0] == '9' || isDelimiter(str[0]) == true)
+        return false;
+    return true;
 }
 
 void parse(char *str, int *keywordCount, int *operatorCount, int *identifierCount, int *newlineCount) {
-    int left = 0, right = 0;
     int len = strlen(str);
+    int i = 0;
 
-    while (right <= len && left <= right) {
-        if (isDelimiter(str[right]) == false)
-            right++;
-
-        if (isDelimiter(str[right]) == true && left == right) {
-            if (isOperator(str[right]) == true)
-                (*operatorCount)++;
-
-            right++;
-            left = right;
-        } else if (isDelimiter(str[right]) == true && left != right
-                   || (right == len && left != right)) {
-            char *subStr = subString(str, left, right - 1);
-
-            if (isKeyword(subStr) == true)
-                (*keywordCount)++;
-
-            else if (validIdentifier(subStr) == true
-                     && isDelimiter(str[right - 1]) == false)
-                (*identifierCount)++;
-
-            left = right;
+    while (i < len) {
+        // Skip whitespace
+        while (i < len && str[i] == ' ') {
+            i++;
         }
-        
-        if (str[right] == '\n') {
+
+        if (isDelimiter(str[i]) == true) {
+            // Check if it's an operator
+            if (isOperator(str[i])) {
+                (*operatorCount)++;
+            }
+            i++;
+        } else {
+            // It's not a delimiter, it's part of an identifier or keyword
+            int start = i;
+            while (i < len && !isDelimiter(str[i])) {
+                i++;
+            }
+            int end = i - 1;
+            char token[50]; // Assuming max token length is 50
+            strncpy(token, str + start, end - start + 1);
+            token[end - start + 1] = '\0';
+
+            if (isKeyword(token)) {
+                (*keywordCount)++;
+            } else if (validIdentifier(token)) {
+                (*identifierCount)++;
+            }
+        }
+
+        // Check for newline
+        if (i < len && str[i] == '\n') {
             (*newlineCount)++;
+            i++;
         }
     }
-    return;
 }
 
 int main() {
-    char str[100] = "int a = b + 1c; while(i<6){break;	}";
+    char str[100] = "int a = b + mayank; while(i<6){break;   }\n";
 
     int keywordCount = 0, operatorCount = 0, identifierCount = 0, newlineCount = 0;
     parse(str, &keywordCount, &operatorCount, &identifierCount, &newlineCount);
@@ -137,5 +98,5 @@ int main() {
     printf("Number of identifiers: %d\n", identifierCount);
     printf("Number of newlines: %d\n", newlineCount);
 
-    return (0);
+    return 0;
 }
