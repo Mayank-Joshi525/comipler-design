f7222f4b07175b6dbcdc43fba28dd73fdf05c434
Mayank Joshi <80774461+Mayank-Joshi525@users.noreply.github.com>
2024-03-17 11:23:46 +0530

    Create Re to Nfa.c

---
+++ Re to Nfa.c
@@ -0,0 +1,40 @@
+// RE TO NFA 
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    char reg[5];
+    int q[5][3] = {0}; // Transition table
+    int j = 1;
+
+    scanf("%s", reg);
+    printf("Given regular expression: %s\n", reg);
+
+    // Constructing the transition table
+    if (reg[0] == 'a') {
+        q[0][0] = 1;
+        q[1][2] = 1;
+    }
+
+    if (reg[1] == 'b') {
+        q[1][1] = 2;
+    }
+
+    // Printing the transition table
+    printf("\n\tTransition Table\n");
+    printf("_____________________\n");
+    printf("Current State | Input | Next State\n");
+    printf("_____________________\n");
+
+    for (int i = 0; i < 3; i++) {
+        if (q[i][0] != 0) printf("\n  q[%d]       |   a   |  q[%d]", i, q[i][0]);
+        if (q[i][1] != 0) printf("\n  q[%d]       |   b   |  q[%d]", i, q[i][1]);
+        if (q[i][2] != 0) printf("\n  q[%d]       |   e   |  q[%d]", i, q[i][2]);
+    }
+
+    printf("\n_____________________\n");
+
+    return 0;
+}
+
+// INPUT a*b 
