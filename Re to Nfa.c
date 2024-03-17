4193f17546619bc079bea36b950f66e91eae923e
Mayank Joshi <80774461+Mayank-Joshi525@users.noreply.github.com>
2024-03-17 12:39:53 +0530

    Update Re to Nfa.c

--- Re to Nfa.c
+++ Re to Nfa.c
@@ -1,10 +1,10 @@
-// RE TO NFA 
+// RE TO NFA
 #include<stdio.h>
 #include<string.h>
 
 int main() {
     char reg[5];
-    int q[5][3] = {0}; // Transition table
+    int q[4][3] = {0}; // Transition table
     int j = 1;
 
     scanf("%s", reg);
@@ -13,28 +13,25 @@ int main() {
     // Constructing the transition table
     if (reg[0] == 'a') {
         q[0][0] = 1;
-        q[1][2] = 1;
     }
 
-    if (reg[1] == 'b') {
-        q[1][1] = 2;
-    }
+    q[1][1] = 2;
+    q[2][2] = 3;
 
     // Printing the transition table
-    printf("\n\tTransition Table\n");
+    printf("\n\tNFA Transition Table for a*b\n");
     printf("_____________________\n");
     printf("Current State | Input | Next State\n");
     printf("_____________________\n");
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 4; i++) {
         if (q[i][0] != 0) printf("\n  q[%d]       |   a   |  q[%d]", i, q[i][0]);
         if (q[i][1] != 0) printf("\n  q[%d]       |   b   |  q[%d]", i, q[i][1]);
-        if (q[i][2] != 0) printf("\n  q[%d]       |   e   |  q[%d]", i, q[i][2]);
+        if (q[i][2] != 0) printf("\n  q[%d]       |   ε   |  q[%d]", i, q[i][2]);
     }
 
     printf("\n_____________________\n");
 
     return 0;
 }
-
-// INPUT a*b 
+//INPUT a*b
