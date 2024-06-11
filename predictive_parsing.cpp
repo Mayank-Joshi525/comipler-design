907f97d714ff006b62aa993f2fc3bc66232e182a
Mayank Joshi <80774461+Mayank-Joshi525@users.noreply.github.com>
2024-06-11 19:14:46 +0530

    Update predictive_parsing.cpp

--- predictive_parsing.cpp
+++ predictive_parsing.cpp
@@ -1,135 +1,80 @@
-#include<iostream> 
-#include<string> 
-#include<deque> 
-using namespace std; 
-int n,n1,n2; 
-int getPosition(string arr[], string q, int size){ 
-    for(int i=0;i<size;i++){ 
-        if(q == arr[i]) 
-            return i; 
-    } 
-    return -1; 
-} 
-int main(){ 
-    string prods[10],first[10],follow[10],nonterms[10],terms[10]; 
-    string pp_table[20][20] = {}; 
-    cout<<"Enter the number of productions : "; 
-    cin>>n; 
-cin.ignore(); 
-cout<<"Enter the productions"<<endl; 
-    for(int i=0;i<n;i++){ 
-getline(cin,prods[i]); 
-        cout<<"Enter first for "<<prods[i].substr(3)<<" : "; 
-getline(cin,first[i]); 
-    } 
-    cout<<"Enter the number of Terminals : "; 
-    cin>>n2; 
-cin.ignore(); 
-cout<<"Enter the Terminals"<<endl; 
-    for(int i=0;i<n2;i++){ 
-cin>>terms[i]; 
-    } 
-    terms[n2] = "$"; 
-    n2++; 
-    cout<<"Enter the number of Non-Terminals : "; 
-    cin>>n1; 
-cin.ignore(); 
-    for(int i=0;i<n1;i++){ 
-        cout<<"Enter Non-Terminal : "; 
-getline(cin,nonterms[i]); 
-        cout<<"Enter follow of "<<nonterms[i]<<" : "; 
-getline(cin,follow[i]); 
-    }cout<<endl; 
-cout<<"Grammar"<<endl; 
-    for(int i=0;i<n;i++){ 
-cout<<prods[i]<<endl; 
-    } 
-for(int j=0;j<n;j++){ 
-
+#include<stdio.h> 
+#include<conio.h> 
+#include<string.h> 
+char prol[7][10]={"s","A","A","B","B","C","C"}; 
+char pror[7][10]={"Aa","Bb","Cd","aB","@","Cc","@"}; 
+char  prod[7][10]={"s-->A","A-->Bb","A-->Cd","B-->aB","B-->@","C-->Cc","C-->@"}; 
+char first[7][10]={"abcd","ab",cd","a@","@","c@","@"}; 
+char follow[7][10]={"$","$","$","a$","b$","c$","d$"}; 
  
-        int row = getPosition(nonterms,prods[j].substr(0,1),n1); 
-        if(prods[j].at(3)!='#'){ 
-            for(int i=0;i<first[j].length();i++){ 
-                int col = getPosition(terms,first[j].substr(i,1),n2); 
-pp_table[row][col] = prods[j]; 
-            } 
-        } 
-        else{ 
-            for(int i=0;i<follow[row].length();i++) 
-            { 
-                int col = getPosition(terms,follow[row].substr(i,1),n2); 
-pp_table[row][col] = prods[j]; 
-            } 
-        } 
-    } 
-    //Display Table 
-    for(int j=0;j<n2;j++){ 
-        cout<<"\t"<<terms[j]; 
-cout<<endl; 
-    for(int i=0;i<n1;i++){ 
-cout<<nonterms[i]<<"\t"; 
-            //Display Table 
-            for(int j=0;j<n2;j++){ 
-cout<<pp_table[i][j]<<"\t"; 
-            } 
-cout<<endl; 
-    } 
-    //Parsing String 
-    char c; 
-    do{ 
-    string ip; 
-    deque<string>pp_stack; 
-pp_stack.push_front("$"); 
-pp_stack.push_front(prods[0].substr(0,1)); 
-    cout<<"Enter the string to be parsed : "; 
-getline(cin,ip); 
-ip.push_back('$'); 
-cout<<"Stack\tInput\tAction"<<endl; 
-    while(true){ 
-       for(int i=0;i<pp_stack.size();i++){ 
-cout<<pp_stack[i]; 
-cout<<"\t"<<ip<<"\t"; 
-        int row1 = getPosition(nonterms,pp_stack.front(),n1); 
-        int row2 = getPosition(terms,pp_stack.front(),n2); 
-        int column = getPosition(terms,ip.substr(0,1),n2); 
-        if(row1 != -1 && column != -1){ 
-            string p = pp_table[row1][column]; 
-            if(p.empty()){ 
-cout<<endl<<"String cannot be Parsed."<<endl; 
-                break; 
-
+Dept. of CSE 17  
+char table[5][6][10]; 
+{ 
+switch(c) 
+{ 
+case 'S':return0; 
+case 'A':return1; 
  
-            } 
-pp_stack.pop_front(); 
-            if(p[3] != '#'){ 
-                for(int x=p.size()-1;x>2;x--){ 
-pp_stack.push_front(p.substr(x,1)); 
-                } 
-            } 
-            cout<<p; 
-        } 
-        else{ 
-            if(ip.substr(0,1) == pp_stack.front()){ 
-                if(pp_stack.front() == "$") 
-                { 
-cout<<endl<<"String Parsed."<<endl; 
-                    break; 
-                } 
-cout<<"Match "<<ip[0]; 
-pp_stack.pop_front(); 
-ip = ip.substr(1); 
-            } 
-            else{ 
-cout<<endl<<"String cannot be Parsed."<<endl; 
-                break; 
-            } 
-        } 
-cout<<endl; 
-    } 
-    cout<<"Continue?(Y/N) "; 
-    cin>>c; 
-cin.ignore(); 
-    }while(c=='y' || c=='Y'); 
-    return 0; 
+case 'B':return2; 
+case 'C':return3; 
+case 'a':return0; 
+case 'b':return1; 
+case 'c':return2; 
+case 'd':return3; 
+case '$':return4; 
 } 
-
+retun(2); 
+} 
+void main() 
+{ 
+int i,j,k; 
+clrscr(); 
+for(i=0;i<5;i++) 
+for(j=0;j<6;j++) 
+strcpy(table[i][j]," "); 
+printf("\n The following is the predictive parsing table for the following grammar:\n"); 
+for(i=0;i<7;i++) 
+printf("%s\n",prod[i]); 
+printf("\n Predictive parsing table is:\n "); 
+fflush(stdin); 
+for(i=0;i<7;i++) 
+{ 
+k=strlen(first[i]); 
+for(j=0;j<10;j++) 
+if(first[i][j]!='@') 
+Dept. of CSE 18  
+strcpy(table[numr(prol[i][0])+1][numr(first[i][j])+1],prod[i]); 
+} 
+for(i=0;i<7;i++) 
+{ 
+if(strlen(pror[i])==1) 
+{ 
+if(pror[i][0]=='@') 
+{ 
+k=strlen(follow[i]); 
+ 
+ 
+for(j=0;j<k;j++) 
+strcpy(table[numr(prol[i][0])+1][numr(follow[i][j])+1]prod[i]); 
+} 
+} 
+} 
+strcpy(table[0][0]," "); 
+strcpy(table[0][1],"a"); 
+strcpy(table[0][2],"b"); 
+strcpy(table[0][3],"c"); 
+strcpy(table[0][4],"d"); 
+strcpy(table[0][5],"$"); 
+strcpy(table[1][0],"S"); 
+strcpy(table[2][0],"A"); 
+strcpy(table[3][0],"B"); 
+strcpy(table[4][0],"C"); 
+printf("\n \n"); 
+for(i-0;i<5;i++) 
+for(j=0;j<6;j++) 
+{ 
+printf("%s_10S",table[i][j]); 
+if(j==5) 
+printf("\n \n"); 
+} 
+getch();
